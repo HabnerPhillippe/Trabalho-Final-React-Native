f72795a58fa9f2e0deb42347b4c291e198016fcd
KauanOliveira1 <oliveirakauan7@gmail.com>
2022-11-19 16:25:06 -0300

    criando rotas

---
+++ src/screens/Home/index.js
@@ -0,0 +1,42 @@
+import { MainContainer } from "../../components/MainContainer/styles.js";
+import { Header } from "../../components/Header/index.js";
+import { useNavigation } from "@react-navigation/native";
+import { PlusButton } from "../../components/PlusButton/index.js";
+import { Api } from "../../services";
+import { FlatList } from "react-native";
+import { ItemCategory } from "../../components/ItemCategory/index.js";
+import { useState, useEffect } from "react";
+
+export const Home = ()=> {
+    const navigation = useNavigation();
+    const [product, setProduct] = useState([]);
+
+    const renderItem = ({ item }) => (
+        <ItemCategory name={item.nome} photo={item.foto} />
+      );
+
+      useEffect(() => {
+        getProdutos();
+      }, [product]);
+
+      const getProdutos= async () => {
+        const { data } = await Api.get("/produto");
+        setCategory(data);
+      };
+    
+      function goBack() {
+        navigation.goBack();
+      };
+
+  return (
+    <MainContainer>
+      <Header title={"produtos"} iconName={"arrow-back"} goBack={goBack} />
+      <PlusButton onPress={() => navigation.navigate("CategoryRegister")} />
+      <FlatList
+        data={product}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+      />
+    </MainContainer>
+  )
+}
---
+++ src/screens/Splash/index.js
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { MainContainer } from "../../components/MainContainer/styles.js";
+import { useNavigation } from "@react-navigation/native";
+
+export const Splash=()=> {
+    
+    const navigation = useNavigation();
+
+  return (
+    <MainContainer>
+      <Text>index</Text>
+    </MainContainer>
+  )
+}
